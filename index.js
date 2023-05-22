fc1170b59763d7382f17de1ea24abac555393de1
chirag r_s <chiragrs2003@gmail.com>
2023-05-22 22:47:23 +0530

    changes

--- index.js
+++ index.js
@@ -8,3 +8,26 @@ window.addEventListener("load", function () {
     }
   });
 });
+
+//cards
+const next = document.querySelector("#next");
+const prev = document.querySelector("#prev");
+
+function handleScrollNext(direction) {
+  const cards = document.querySelector(".card-content");
+  cards.scrollLeft = cards.scrollLeft +=
+    window.innerWidth / 2 > 600
+      ? window.innerWidth / 2
+      : window.innerWidth - 100;
+}
+
+function handleScrollPrev(direction) {
+  const cards = document.querySelector(".card-content");
+  cards.scrollLeft = cards.scrollLeft -=
+    window.innerWidth / 2 > 600
+      ? window.innerWidth / 2
+      : window.innerWidth - 100;
+}
+
+next.addEventListener("click", handleScrollNext);
+prev.addEventListener("click", handleScrollPrev);
